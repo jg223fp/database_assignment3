1e553e5f99f8aa6ef85c5f11126bedfa47a33a39
65yryr <anass98h@gmail.com>
2022-03-09 23:46:42 +0100

    its moslty done

--- src/view.py
+++ src/view.py
@@ -36,62 +36,84 @@ def getMenuChoise(top):
             print("Value must be an integer.") 
             selection = -1
 
-# Prompts for and return a member ID as an int            
-def promptForPlayerId(): 
-    while True:
-        try:    
-            selection = int(input("Enter ID of the player which bag you want to look in: "))
-            return selection
-        except ValueError:
-            print("Value must be an integer.") 
-            
+
 # Prints out player data          
 def presentPlayers(cursor):
-    print("| {:<15} | {:<20} | {:<15} | {:<15} |".format(\
-     "ID",
-     "Name",
-     "Level",
-     "Nationality"))
-    print("-"*80)
-    
+    headings = ['ID','Name','Level','Nationality']
+    player_ids= []
+    players_arry=[]
     for id,name,nationality,level in cursor:
-        print("| {:<15} | {:<20} | {:<15} | {:<15} |".format(\
-     id,
-     name,
-     nationality,
-     level))
+        players_arry.append([id,name,nationality,level])
+        player_ids.append(id)
+    players_arry_layout = [
+        [sg.Table(values=players_arry, headings=headings, max_col_width=35,
+                    auto_size_columns=True,
+                    justification='right',
+                    num_rows=10,
+                    key='-TABLE-',
+                    row_height=35,
+                    tooltip='Reservations Table')],
+        [sg.Text("Choose a player id from the list")],
+        [sg.Listbox(values=player_ids, size=(40, 5), select_mode='single', key='-playeslected-')],
+        [sg.Button('show the bag'),sg.Exit()],
+        ]
+    players_arry_window = sg.Window("Contact Information Window", 
+    players_arry_layout, modal=True)
+    event, values = players_arry_window.read()
+    if event == "Exit" or event == sg.WIN_CLOSED:
+        players_arry_window.close()
+    elif event == 'show the bag':
+        players_arry_window.close()
+        return values['-playeslected-'][0]
 
 # prints the bag of a player
 def showBag(cursor,name):
-    print("\n {}s bag".format(name))
-    print("| {:<15} | {:<15} | {:<7} | {:<5} | {:<5} | {:<5} | {:<5} | {:<15} |".format(\
-     "Disc name",
-     "plastic",
-     "weigth(g)",
-     "speed",
-     "glide",
-     "turn",
-     "fade",
-     "classification"))
-    print("-"*80)
-    
+    headings = ["Disc name","plastic","weigth(g)","speed","glide","turn","fade","classification"]
+    players_arry=[]
     for name,plastic,weigth,speed,glide,turn,fade,classification in cursor:
-        print("| {:<15} | {:<15} | {:<7}   | {:<5} | {:<5} | {:<5} | {:<5} | {:<15} |".format(\
-     name,
-     plastic,
-     weigth,
-     speed,
-     glide,
-     turn,
-     fade,
-     classification))
- 
+        players_arry.append([name,plastic,weigth,speed,glide,turn,fade,classification])
+    players_arry_layout = [[sg.Text("{}s bag".format(name))],
+        [sg.Table(values=players_arry, headings=headings, max_col_width=35,
+                    auto_size_columns=True,
+                    justification='right',
+                    num_rows=10,
+                    key='-TABLE-',
+                    row_height=35,
+                    tooltip='Reservations Table')],
+        [sg.Button('return to main menu')],
+        ]
+    players_arry_window = sg.Window("Contact Information Window", 
+    players_arry_layout, modal=True)
+    event, values = players_arry_window.read()
+    if event == "Exit" or event == sg.WIN_CLOSED:
+        players_arry_window.close()
+    elif event == 'return to main menu':
+        players_arry_window.close()
+
+
 #present competitions and returns a list with the name and year of the competition       
 def presentCompetitions(competitions):
-    i = 1
+    competitions_list = []
+    competitions_year = []
     for c in competitions:
-        print("{}. {} {}".format(i, c[0],c[1]))
-        i += 1
+        competitions_list.append(c[0])
+        competitions_year.append(c[1])
+    competitions_arry_layout = [ [[sg.Text("select a competition to view the winner")],
+        [sg.Listbox(values=competitions_list, size=(40, 5), select_mode='single', key='-competitionsslected-')],
+        [sg.Listbox(values=competitions_year, size=(40, 5), select_mode='single', key='-yearslected-')]],
+        [sg.Button('show the winner'),sg.Exit()],
+        ]
+    competitions_arry_window = sg.Window("Contact Information Window", 
+    competitions_arry_layout, modal=True)
+    event, values = competitions_arry_window.read()
+    if event == "Exit" or event == sg.WIN_CLOSED:
+        competitions_arry_window.close()
+    elif event == 'show the winner':
+        competitions_arry_window.close()
+        print([values['-competitionsslected-'][0],values['-yearslected-'][0]])
+        return [values['-competitionsslected-'][0],values['-yearslected-'][0]]
+
+
 
 def presentWinner(winner):
     print(winner)
